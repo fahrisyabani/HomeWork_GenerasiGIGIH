88f0e74a7d347e38bb351b897c73f490092d7678
fahrisyabani <fahrisyabanipd@gmail.com>
2022-03-28 11:46:10 +0700

    Add List and Looping

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Title from './components/title/title.js'
 import Card from './components/card/card.js';
 import Waves from './components/waves/waves';
+import Looping from './HomeWork6/Home/track';
+import Title2 from './HomeWork6/Home/title';
 
 function App() {
 
@@ -30,6 +32,27 @@ function App() {
         {/* Footer */}
         <Waves />
         {/* Akhir Footer */}
+
+        {/* Batas Homework-6 */}
+        <div className='Music'>
+          <section className='track'>
+            <div className='container pt-3'>
+
+              {/* Title */}
+              <Title2 />
+              {/* akhir Title */}
+
+              <div className="col-md-3">
+              <hr></hr>
+              </div>
+
+              {/* Track */}
+                <Looping />
+              {/* akhir Track */}
+
+            </div>
+          </section>
+        </div>
 
     </div>
   );
---
+++ src/HomeWork6/Home/title/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Title2() {
+
+    return (
+        <div className="row">
+        <div className="col-md-3 pb-2">
+          <p className="start">Playlist</p>
+          <h1><span>Made for You</span></h1>
+        </div>
+      </div>
+    )
+}
+
+export default Title2
