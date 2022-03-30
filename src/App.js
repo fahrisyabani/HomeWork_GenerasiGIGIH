4ce01868b70ecafaee6e85e3da07e88e32a579db
fahrisyabani <fahrisyabanipd@gmail.com>
2022-03-30 16:16:54 +0700

    Update halaman spotify, dengan mengambil data dari spotify serta bisa mensearch lagu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-import Title from './components/title/title.js'
+import Title from './components/title/title'
 import Card from './components/card/card.js';
 import Waves from './components/waves/waves';
 import Looping from './HomeWork6/Home/track';
 import Title2 from './HomeWork6/Home/title';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Login from './Login'
+import Dashboard from "./Dashboard"
+
+const code = new URLSearchParams(window.location.search).get("code")
 
 function App() {
 
@@ -33,7 +38,7 @@ function App() {
         <Waves />
         {/* Akhir Footer */}
 
-        {/* Batas Homework-6 */}
+        {/* Homework-6 */}
         <div className='Music'>
           <section className='track'>
             <div className='container pt-3'>
@@ -53,6 +58,29 @@ function App() {
             </div>
           </section>
         </div>
+        {/* Batas Homework-6 */}
+
+        {/* HomeWork-7 */}
+        <div className='Music'>
+          <section className='track'>
+            <div className='container pt-3'>
+
+              {/* Title */}
+              <Title2 />
+              {/* akhir Title */}
+
+              <div className="col-md-3">
+              <hr></hr>
+              </div>
+
+              {/* Track */}
+              code ? <Dashboard code={code} /> : <Login />
+              {/* akhir Track */}
+
+            </div>
+          </section>
+        </div>
+        {/* Batas HomeWork-7 */}
 
     </div>
   );
